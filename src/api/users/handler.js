06e518da3bab93f20cecfc43d5190928b58de8b4
rafienajwan <rafie.anjasmara@gmail.com>
2025-02-04 16:11:25 +0700

    add users handler for creating and retrieving user data

---
+++ src/api/users/handler.js
@@ -0,0 +1,49 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addUser({ username, password, fullname });
+
+    if (!userId) { // ClientError akan terjadi jika userId bernilai falsy
+      throw new ClientError('Gagal menambahkan user. Mohon coba lagi');
+    }
+
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getUserByIdHandler(request, h) {
+    const { id } = request.params;
+
+    const user = await this._service.getUserById(id);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+}
+
+module.exports = UsersHandler;
